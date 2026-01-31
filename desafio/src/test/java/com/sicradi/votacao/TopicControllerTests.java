663ab5194b04cd98f9ccdd41edc0a5143d753de3
Gabriel Portugal <gabrielpguadelupe@gmail.com>
2026-01-31 01:24:34 -0300

    feat: teste unitario inicial

--- desafio/src/test/java/com/sicradi/votacao/TopicControllerTests.java
+++ desafio/src/test/java/com/sicradi/votacao/TopicControllerTests.java
@@ -1,13 +1,33 @@
 package com.sicradi.votacao;
 
+import com.sicradi.votacao.interfaces.rest.controller.TopicController;
+import com.sicradi.votacao.interfaces.rest.dto.TopicRequest;
+import com.sicradi.votacao.interfaces.rest.dto.TopicResponse;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-class VotacaoApplicationTests {
+class TopicControllerTests {
+
+	@Autowired
+	private TopicController topicController;
 
 	@Test
-	void contextLoads() {
-	}
+	void deveCriarTopicComSucesso() {
+		TopicRequest request = new TopicRequest();
+		request.setTitle("Pauta Teste");
+		request.setDescription("Descrição da pauta de teste");
 
+		ResponseEntity<TopicResponse> response = topicController.createTopic(request);
+
+		assertThat(response.getStatusCode()).isEqualTo(org.springframework.http.HttpStatus.OK); // Se usar ResponseEntity.created, troque para HttpStatus.CREATED
+		TopicResponse topic = response.getBody();
+		assertThat(topic).isNotNull();
+		assertThat(topic.getTitle()).isEqualTo("Pauta Teste");
+		assertThat(topic.getDescription()).isEqualTo("Descrição da pauta de teste");
+		assertThat(topic.getId()).isNotNull();
+	}
 }

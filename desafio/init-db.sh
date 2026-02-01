25249799689191d0f3c551df6227d58aafc777ca
Gabriel Portugal <gabrielpguadelupe@gmail.com>
2026-02-01 11:52:36 -0300

    feat: docker

--- desafio/init-db.sh
+++ desafio/init-db.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 set -e
+
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE DATABASE "votacao-teste";
+    CREATE DATABASE IF NOT EXISTS votacao;
+    CREATE DATABASE IF NOT EXISTS "votacao-teste";
 EOSQL
